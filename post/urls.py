48989dde0b6bcf919fae596f661815014a41a559
Abdessamad Pas <boughadoin@gmail.com>
2022-12-31 23:44:16 +0100

    change the arg type  for urls of post app

--- post/urls.py
+++ post/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
   #  path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('', views.index, name='index'),
     path('category/<slug:category_slug>/', views.category, name='category'),
-    path('tags/<str:tag_slug>/', views.tags, name='tag'),
-    path('post/<str:slug>/', views.PostDetail, name='post_detail'),
+    path('tags/<slug:tag_slug>/', views.tags, name='tag'),
+    path('post/<slug:slug>/', views.PostDetail, name='post_detail'),
 
 ]
