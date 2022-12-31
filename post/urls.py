d2776fd0cfe2f97da866767dd6931b0bc5616fbf
Abdessamad Pas <boughadoin@gmail.com>
2022-12-31 17:55:27 +0100

    configure static and template setting

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('hello/', views.index, name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
