9aa0700906c21e76fc4a1301bf55323edec1ea18
Abdessamad Pas <boughadoin@gmail.com>
2022-12-31 21:36:28 +0100

    customize admin dashboard

--- post/admin.py
+++ post/admin.py
@@ -4,16 +4,20 @@ from .models import *
 
 
 # Register your models here.
-class CategoryArticleAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-class PostArticleAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-class TagArticleAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter= ('status', 'category', 'tags')
+    list_display = ('title', 'status', 'category', 'publication_date')
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
 
 
 
-admin.site.register(Category, CategoryArticleAdmin)
-admin.site.register(Tag, TagArticleAdmin)
-admin.site.register(Post, TagArticleAdmin)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
