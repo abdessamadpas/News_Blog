c61b2bf63d1c6a89e824fd179b0ad3190d6ef6ff
Abdessamad Pas <boughadoin@gmail.com>
2022-12-31 17:20:26 +0100

    add templates and static files

---
+++ blog/static/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
